9f6cac42c5e92e01011f9c62cfcf9bb0fb1ed8d6
lilpolymath <ayofavour7@gmail.com>
2020-09-15 16:32:08 +0100

    feat: button click and modal animation

--- script.js
+++ script.js
@@ -2,14 +2,43 @@ const paste = document.getElementById('paste');
 const download = document.getElementById('download');
 const cancel = document.getElementById('cancel');
 const modal = document.querySelector('.modal');
+const main = document.querySelectorAll('.button');
 
 const pasteClick = () => {
-  modal.classList.add('show');
+  console.log('p');
+  paste.textContent = 'Fetching Video';
+  main[0].classList.add('animate_button');
+  paste.addEventListener(
+    'transitionend',
+    e => {
+      if (e.pseudoElement == '::after') {
+        modal.classList.add('show');
+        console.log('show');
+      }
+    },
+    true
+  );
+};
+
+const downloadClick = () => {
+  console.log('d');
+  download.textContent = 'Downloading';
+  main[1].classList.add('animate_button');
+  download.addEventListener('transitionend', e => {
+    if (e.pseudoElement == '::after') {
+      download.textContent = 'Downloaded';
+      cancel.textContent = 'Go Home';
+    }
+  }, true);
 };
 
 const closeModal = () => {
+  console.log('c');
+  paste.textContent = 'Paste Link';
+  main.forEach(elem => elem.classList.remove('animate_button'));
   modal.classList.remove('show');
 };
 
-cancel.addEventListener('click', closeModal);
-paste.addEventListener('click', pasteClick);
+cancel.addEventListener('click', closeModal, true);
+paste.addEventListener('click', pasteClick, true);
+download.addEventListener('click', downloadClick, true);
